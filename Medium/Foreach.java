1814b56f6fed387771302e8df455b0d1afdc5f18
MauryAzura <azuralynux@gmail.com>
2024-01-30 14:37:47 -0600

    Commit 5

---
+++ Medium/Foreach.java
@@ -0,0 +1,30 @@
+package Medium;
+
+public class Foreach {
+    
+    
+    public static void main(String[] args) {
+        
+        String[] name = {"Mau","Luis","Jose","Pedro","Luisa"};
+
+        //Usando lenght
+
+        for(int i=0;i<name.length;i++){
+
+            System.out.println(name[i]);
+        
+        }
+
+        //Usando foreach
+
+        System.out.println("-----------Foreach------------");
+
+        for(String i: name){
+
+            System.out.println(i);
+
+        }
+
+    }
+
+}

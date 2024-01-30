f635a24df855719ab587a5f5a0e518befe2f5d13
MauryAzura <azuralynux@gmail.com>
2024-01-30 14:02:38 -0600

    Commit 2

--- aleatorio.java
+++ aleatorio.java
@@ -7,19 +7,20 @@ public class aleatorio {
         byte aleatorio = (byte) (Math.random()*100);
         byte c=0;
         byte n_i = 0;
+        String m=" ";
 
         while(true){
 
-            n_i = Byte.parseByte(JOptionPane.showInputDialog("---Adivina el numero--- \n Ingresa un numero:"));
+            n_i = Byte.parseByte(JOptionPane.showInputDialog("---Adivina el numero--- \n"+m+"\n Ingresa un numero:"));
 
             if(n_i<aleatorio){
 
-                System.out.println("El numero es mayor");
+                m="El numero es mayor";
                 c++;
 
             }else if(n_i>aleatorio){
 
-                System.out.println("El numero es menor");
+                m="El numero es menor";
                 c++;
 
             }else{
@@ -27,7 +28,6 @@ public class aleatorio {
                 JOptionPane.showMessageDialog(null,"Los has logrado el numero es "+aleatorio+" te ha tomado "+c+" intentos");
                 break;
             }
-            System.out.println(aleatorio);
         }
 
 

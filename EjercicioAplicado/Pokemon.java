6800a4c1e372d202cf83cf296e49d2f472307598
MauryAzura <azuralynux@gmail.com>
2024-02-06 20:18:28 -0600

    Commit 8

---
+++ EjercicioAplicado/Pokemon.java
@@ -0,0 +1,15 @@
+package EjercicioAplicado;
+
+public abstract class Pokemon {
+    
+    protected int pokedex_num;
+    protected String nombre;
+    protected double peso;
+    protected String sexo;
+    protected int temporada;
+
+    protected abstract void atacarPlacaje();
+
+    
+
+}
